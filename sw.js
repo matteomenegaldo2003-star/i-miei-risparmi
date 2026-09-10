31d66000b378198f5019b6e6c54b92a2e21ff351
matteomenegaldo2003-star <matteomenegaldo2003@gmail.com>
2026-09-11 00:17:10 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "i-miei-risparmi-v12";
+const CACHE = "i-miei-risparmi-v13";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icons/icon-180.png", "./icons/icon-192.png", "./icons/icon-512.png"];
 
 self.addEventListener("install", event => {
